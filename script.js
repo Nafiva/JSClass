2ac83c2d9e10f2524192847219e2bc21537c4037
Nafiva <jezanafi@gmail.com>
2023-06-27 19:07:47 -0700

    adding profile text

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName = "Nafiva";
+var lastName = "Jeza";
+var yearsOfStudy = " 4 ";
+var goal = "My goal after completing this program is to break into the tech industry as a Front-End Developer that contributes to AI.";
+
+var profile = "My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " months. " + goal ;
+
+  document.write(profile); 
